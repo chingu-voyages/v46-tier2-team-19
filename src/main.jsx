3daa135585a3594a3c85e70ff56da84561d173bd
timDeHof <this.is.tim.dehof@gmail.com>
2023-10-11 19:17:55 -0400

    style(creating a styles folder): change name of 'index.css' to 'global.css' and moved to folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/global.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./api/query-client";
