7bee5794a83be9cd852c13bf5d4dab7d743e6b3f
timDeHof <this.is.tim.dehof@gmail.com>
2023-10-19 13:03:48 -0400

    chore(vite.config.js): load environment variables using dotenv in vite.config.js
    The dotenv package is used in vite.config.js to these environment variables. In package.json, `npm
    run cz` is removed from pre-commit hooks to streamline the commit process. In api/recipes.js, the
    hardcoded API key and host are replaced with environment variables to enhance security and
    flexibility. In recipe-card.jsx, the recipe card is wrapped in a Link component to prepare for
    future implementation of routing to a detailed recipe page.
    
    CH-12

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -25,8 +25,10 @@ export async function fetchRecipes(search) {
       `https://tasty.p.rapidapi.com/recipes/list`,
       {
         headers: {
-          "X-RapidAPI-Key": import.meta.env.TASTY_API_KEY,
-          "X-RapidAPI-Host": import.meta.env.TASTY_API_URL,
+          // "X-RapidAPI-Key":
+          //   "c222131657msh9269f1c4dcb2ea4p1ed181jsn7bfd8bcf97a9",
+          "X-RapidAPI-Key": import.meta.env.VITE_TASTY_API_KEY,
+          "X-RapidAPI-Host": "tasty.p.rapidapi.com",
         },
         params: { from: "0", size: "8", q: search },
       },
--- src/features/recipes/components/recipe-card/recipe-card.jsx
+++ src/features/recipes/components/recipe-card/recipe-card.jsx
@@ -1,30 +1,35 @@
 /* eslint-disable react/prop-types */
 import { StarRating } from "@/features/ui";
+import { Link } from "react-router-dom";
 export const RecipeCard = ({
   thumbnail_url,
   name,
   thumbnail_alt_text,
   num_servings,
   user_ratings,
+  canonical_id,
 }) => {
+  console.log(canonical_id);
   return (
     <div className="w-full rounded-2xl bg-white sm:w-[311px] shadow-xl">
-      <div className="h-[150px] w-full">
-        <img
-          src={thumbnail_url}
-          alt={thumbnail_alt_text}
-          className="object-cover w-full h-full rounded-t-2xl"
-        />
-      </div>
-      <div className="flex flex-col w-full gap-12 mb-2 text-left px-7">
-        <h3 className="text-[14px] font-bold text-gray-900">{name}</h3>
-        <div className="flex flex-wrap justify-between">
-          <p className="text-[12px]">{`${num_servings} serving(s)`}</p>
-          <div>
-            <StarRating {...user_ratings} />
+      <Link to="#">
+        <div className="h-[150px] w-full">
+          <img
+            src={thumbnail_url}
+            alt={thumbnail_alt_text}
+            className="object-cover w-full h-full rounded-t-2xl"
+          />
+        </div>
+        <div className="flex flex-col w-full gap-12 mb-2 text-left px-7">
+          <h3 className="text-[14px] font-bold text-gray-900">{name}</h3>
+          <div className="flex flex-wrap justify-between">
+            <p className="text-[12px]">{`${num_servings} serving(s)`}</p>
+            <div>
+              <StarRating {...user_ratings} />
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
