3c28604f6d35022cdb55178594abab6464d952a0
joekotvas <joseph.kotvas@gmail.com>
2023-10-19 06:28:25 -0500

    refactor(rename nav to spa): rename Nav to Spa
    Change naming to avoid confusion with the semantic html <nav> tag

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header, Footer } from "@/features/ui";
 
-export default function Nav() {
+export default function Spa() {
   return (
     <>
       <Header />
