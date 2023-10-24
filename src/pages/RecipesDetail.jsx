fc8924cd3e1c10bec78f322390588de715c30f79
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-10-24 13:47:59 -0400

    refactor(recipesdetail.jsx): refactor to use Heading component
    Refactor to use Heading component
    
    CH-60

--- src/pages/RecipesDetail.jsx
+++ src/pages/RecipesDetail.jsx
@@ -1,7 +1,12 @@
+import { Heading } from "@/features/ui";
+
 const RecipesDetailPage = () => {
   return (
     <div className="p-10">
-      <h1 className="text-center">Recipes Detail Page</h1>
+      {/* <h1 className="text-center">Recipes Detail Page</h1> */}
+      <Heading level="h1" variant="lava">
+        Recipes Detail Page
+      </Heading>
     </div>
   );
 };
