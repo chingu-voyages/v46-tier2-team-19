80e61d7a032209b749aaf069f2e81341fe8e5720
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-10-24 18:13:56 -0400

    refactor(recipe-card.jsx): refactor to use Heading component
    Recfator to use Heading component
    
    CH-60

--- src/features/recipes/components/recipe-card/recipe-card.jsx
+++ src/features/recipes/components/recipe-card/recipe-card.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { StarRating } from "@/features/ui";
+import { Heading, StarRating } from "@/features/ui";
 import { Link } from "react-router-dom";
 export const RecipeCard = ({
   thumbnail_url,
@@ -21,7 +21,10 @@ export const RecipeCard = ({
           />
         </div>
         <div className="flex flex-col w-full gap-12 mb-2 text-left px-7">
-          <h3 className="text-[14px] font-bold text-gray-900">{name}</h3>
+          {/* <h3 className="text-[14px] font-bold text-gray-900">{name}</h3> */}
+          <Heading level="h3" variant="lava">
+            {name}
+          </Heading>
           <div className="flex flex-wrap justify-between">
             <p className="text-[12px]">{`${num_servings} serving(s)`}</p>
             <div>
