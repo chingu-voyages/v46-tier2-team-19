1bf9cc734eddaa919499cb83ea766b8b6675a095
joekotvas <joseph.kotvas@gmail.com>
2023-11-02 07:07:50 -0500

    refactor(home page features): rename folders to change case (Step 2 of 2)

--- src/features/HomePage/index.js
+++ src/features/HomePage/index.js
@@ -1,3 +1,3 @@
-export * from "./components/hero-section2";
-export * from "./components/search-section2";
-export * from "./components/top-recipes2";
+export * from "./components/hero-section";
+export * from "./components/search-section";
+export * from "./components/top-recipes";
