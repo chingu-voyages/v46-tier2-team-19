a82296832770e8eace4dd6cac7a4cfd332425d25
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-11-03 17:54:17 -0400

    feat(app.jsx): add Favorites page route
    Add Favorites page route.
    
    CH-78

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Spa from "./routes";
 import makeServer from "./mirageServer/server";
 import { Routes, Route } from "react-router-dom";
 import { Home, About, Search, RecipeDetails, Components } from "@/pages";
+import Favorites from "./pages/Favorites";
 // Please keep on when possible to avoid using up our Tasty API free quota
 // All api calls will be intercepted and fulfilled by the mirage server.
 const USE_MIRAGE_API = true;
@@ -21,6 +22,7 @@ function App() {
           <Route path=":recipeId" element={<RecipeDetails />} />
         </Route>
         <Route path="/components" element={<Components />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Route>
     </Routes>
   );
