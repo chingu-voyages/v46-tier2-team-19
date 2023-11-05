da69610c0c2e18c5daabfc02521618f16e05a996
timDeHof <this.is.tim.dehof@gmail.com>
2023-11-04 20:13:56 -0400

    feat(api): add new tips.js file to fetch tips from Tasty API
    A new file, tips.js, has been added to the API directory. This file contains a function to fetch
    tips from the Tasty API. This function is asynchronous and uses axios to make a GET request to the
    Tasty API. The function takes in three parameters: id, from, and size, with default values for from
    and size. The function returns the data received from the API or logs an error if the request fails.
    This addition will allow the application to fetch and display tips from the Tasty API, enhancing the
    user experience.

---
+++ src/api/tips.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export async function fetchTips(id, from = 0, size = 20) {
+  console.log(`Fetching ${id} tips`);
+  try {
+    const { data } = await axios.get(`https://tasty.p.rapidapi.com/tips/list`, {
+      headers: {
+        "X-RapidAPI-Key": import.meta.env.VITE_TASTY_API_KEY,
+        "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+      },
+      params: { from, size, id: id },
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
