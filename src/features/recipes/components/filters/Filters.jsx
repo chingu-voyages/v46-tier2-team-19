b18f652963479466b87552e24055a311227fc54a
timDeHof <this.is.tim.dehof@gmail.com>
2023-11-08 21:10:45 -0500

    refactor(filters.jsx): simplify filter component and add tag selection functionality
    The Filters component has been simplified by removing the Listbox and Transition components and
    replacing them with a simpler list and button structure. This change makes the component easier to
    understand and maintain. Additionally, a new feature has been added that allows tags to be selected.
    This feature enhances the user experience by allowing users to filter recipes based on selected
    tags.

--- src/features/recipes/components/filters/Filters.jsx
+++ src/features/recipes/components/filters/Filters.jsx
@@ -1,82 +1,42 @@
 import PropTypes from "prop-types";
-import React, { useState } from "react";
-import { Listbox, Transition } from "@headlessui/react";
-import { BiCheck } from "react-icons/bi";
-import { HiChevronUpDown } from "react-icons/hi2";
+import { Heading } from "@/features/ui";
+export const Filters = ({ tagsCollection, onTagSelected, selectedTags }) => {
+  const isTagSelected = (tag) => {
+    return selectedTags.some((selectedTag) => selectedTag.id === tag.id);
+  };
 
-export const Filters = ({ tagsCollection }) => {
   return (
-    <div className="inset-0 flex items-center justify-center">
-      <div className="flex flex-wrap">
-        {Object.entries(tagsCollection).map(([tagType, tags]) => (
-          <div key={tagType}>
-            <h3>{tagType}</h3>
-            <ListSelector tags={tags} />
-          </div>
-        ))}
-      </div>
+    <div className="flex flex-wrap items-start justify-around">
+      {Object.entries(tagsCollection).map(([tagType, tags]) => (
+        <div key={tagType}>
+          <Heading level="h5" variant="tangerine" className="capitalize">
+            {tagType === "cooking_style" ? "Cooking Style" : tagType}
+          </Heading>
+
+          <ul className="space-y-1">
+            {tags.map((tag) => (
+              <li key={tag.id}>
+                <button
+                  onClick={() => onTagSelected(tag)}
+                  className={`border-2 rounded-full px-4 text-sm cursor-pointer ${
+                    isTagSelected(tag)
+                      ? "bg-gradient-tangerine-diagonal text-white"
+                      : "text-tangerine-600  border-tangerine-600"
+                  }  `}
+                >
+                  {tag.display_name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
 
-const ListSelector = ({ tags }) => {
-  const [selectedTag, setSelectedTag] = useState(tags[0]);
-  return (
-    <Listbox value={selectedTag} onChange={setSelectedTag}>
-      <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md curser-default focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-        <span className="block truncate">{selectedTag.DisplayName}</span>
-        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-          <HiChevronUpDown
-            className="w-5 h-5 text-gray-400"
-            aria-hidden="true"
-          />
-        </span>
-      </Listbox.Button>
-      <Transition
-        as={React.Fragment}
-        leave="transition ease-in duration-100"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg outline-none max-h-60 ring-1 ring-black/5 focus: sm:text-sm">
-          {tags.map((tag) => (
-            <Listbox.Option
-              key={tag.id}
-              className={({ active }) =>
-                `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                  active ? "bg-amber-100 text-amber-900" : "text-gray-900"
-                }`
-              }
-              value={tag.displayName}
-            >
-              {({ selected }) => (
-                <>
-                  <span
-                    className={`block truncate ${
-                      selected ? "font-medium" : "font-normal"
-                    }`}
-                  >
-                    {`${tag.displayName} - ${tag.count}`}{" "}
-                  </span>
-                  {selected ? (
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                      <BiCheck className="w-5 h-5" aria-hidden="true" />
-                    </span>
-                  ) : null}
-                </>
-              )}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </Transition>
-    </Listbox>
-  );
-};
-
 Filters.propTypes = {
-  tagsCollection: PropTypes.arrayOf(PropTypes.object),
-};
-
-ListSelector.propTypes = {
-  tags: PropTypes.arrayOf(PropTypes.object),
+  tagsCollection: PropTypes.object,
+  selectedTags: PropTypes.arrayOf(PropTypes.object),
+  onTagSelected: PropTypes.func,
 };
