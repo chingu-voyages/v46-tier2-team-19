0375a277044662816871dd33d885f148048a30d2
timDeHof <this.is.tim.dehof@gmail.com>
2023-11-10 13:39:10 -0500

    feat(spa): add ScrollToTop function to scroll to top on route change
    The ScrollToTop function has been added to ensure that the page automatically scrolls to the top
    when the route changes. This improves user experience as users won't have to manually scroll up
    after navigating to a new page.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header, Footer } from "@/features/ui";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 export default function Spa() {
   return (
     <>
       <Header />
       <main className="w-full mx-auto">
+        <ScrollToTop />
         <Outlet />
       </main>
       <Footer />
