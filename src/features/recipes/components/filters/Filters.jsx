d00242a1b007b0a998debc56543c4a76144e4ba4
timDeHof <this.is.tim.dehof@gmail.com>
2023-11-10 18:30:21 -0500

    refactor(filter.jsx): replace Heading component with h5 tag and adjust styles for better readability
    The Heading component was replaced with a native h5 tag to reduce the number of dependencies and
    improve performance. The styles of the filter buttons were adjusted to improve readability and user
    interaction. The border color and background color were changed to provide better contrast and the
    space between list items was increased for better visual separation.

--- src/features/recipes/components/filters/Filters.jsx
+++ src/features/recipes/components/filters/Filters.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { Heading } from "@/features/ui";
 
 export const Filters = ({ tagsCollection, onTagSelected, selectedTags }) => {
   const isTagSelected = (tag) => {
@@ -10,17 +9,16 @@ export const Filters = ({ tagsCollection, onTagSelected, selectedTags }) => {
     <div className="flex flex-wrap items-start justify-around">
       {Object.entries(tagsCollection).map(([tagType, tags]) => (
         <div key={tagType}>
-          <Heading level="h5" variant="tangerine" className="capitalize">
+          <h5 className="capitalize">
             {tagType === "cooking_style" ? "Cooking Style" : tagType}
-          </Heading>
-
-          <ul className="space-y-1">
+          </h5>
+          <ul className="space-y-2">
             {tags.map((tag) => (
               <li key={tag.id}>
                 <button
                   onClick={() => onTagSelected(tag)}
-                  className={` transition ease-in-out border-2 rounded-full delay-150 px-4 text-sm cursor-pointer bg-tangerine-300 text-tangerine-800 hover:shadow-lg hover:scale-110 ${
-                    isTagSelected(tag) ? "border-tangerine-800" : "border-white"
+                  className={` transition ease-in-out border-[3px] rounded-full delay-150 px-4 text-sm cursor-pointer bg-tangerine-100 text-tangerine-800 hover:shadow-lg hover:scale-110 ${
+                    isTagSelected(tag) ? "border-tangerine-400" : "border-white"
                   }`}
                 >
                   {tag.display_name}
