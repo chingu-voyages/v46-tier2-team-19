fffd05f305d5964837d4c0b02577e46fddc905c6
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2023-11-11 09:03:02 -0500

    feat(favorites.jsx): display confirmation modal on Clear Favorites button click
    Display confirmation modal on Clear Favorites button click
    
    CH-110

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import { Button, Heading } from "@/features/ui";
 import { RecipeCard } from "@/features/recipes/components/recipe-card";
 import usePageTitle from "../hooks/usePageTitle";
@@ -7,6 +8,7 @@ const Favorites = () => {
   usePageTitle("Favorite Recipes");
 
   const [favorites, setFavorites] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const savedFavorites = localStorage.getItem("favorites");
@@ -15,6 +17,8 @@ const Favorites = () => {
       setFavorites(JSON.parse(savedFavorites));
     }
   }, []);
+
+  const hasFavorites = favorites.length > 0;
 
   return (
     <>
@@ -27,14 +31,11 @@ const Favorites = () => {
           variant="secondary"
           size="small"
           onClick={() => {
-            localStorage.removeItem("favorites");
-            setFavorites([]);
+            hasFavorites ? setIsOpen(true) : null;
           }}
         >
-          {favorites.length > 0
-            ? `Clear Favorites ${
-                favorites.length > 0 && `(${favorites.length})`
-              }`
+          {hasFavorites
+            ? `Clear Favorites ${hasFavorites ? `(${favorites.length})` : null}`
             : "No Favorites Saved Yet"}
         </Button>
       </div>
@@ -44,6 +45,77 @@ const Favorites = () => {
           <RecipeCard key={recipeId} {...recipeId} />
         ))}
       </div>
+
+      <Transition show={isOpen} as={Fragment}>
+        <Dialog onClose={() => setIsOpen(false)} className="relative z-50">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            {/* The backdrop, rendered as a fixed sibling to the panel container */}
+            <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+          </Transition.Child>
+
+          {/* Full-screen container to center the panel */}
+          <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              {/* The actual dialog panel  */}
+              <Dialog.Panel className="mx-auto max-w-sm rounded-lg bg-white overflow-hidden">
+                <Dialog.Title className="px-6 py-4 bg-tangerine-700 text-white">
+                  Clear Favorites
+                </Dialog.Title>
+                <hr />
+                <div className="p-6">
+                  <Dialog.Description>
+                    This will permanently remove your favorite recipes.
+                  </Dialog.Description>
+                  <p className="mt-4">
+                    <strong>
+                      Are you sure you want to clear your favorites?
+                    </strong>
+                  </p>
+                </div>
+                <hr />
+                <div className="flex gap-10 p-6">
+                  <Button
+                    variant="secondary"
+                    size="small"
+                    onClick={() => {
+                      localStorage.removeItem("favorites");
+                      setFavorites([]);
+                      setIsOpen(false);
+                    }}
+                    className="w-full place-content-center"
+                  >
+                    Yes
+                  </Button>
+                  <Button
+                    variant="secondary"
+                    size="small"
+                    onClick={() => setIsOpen(false)}
+                    className="w-full place-content-center"
+                  >
+                    No
+                  </Button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   );
 };
