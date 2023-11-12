41c62e689047d24004c336aca78d64a9e07e5dc7
timDeHof <this.is.tim.dehof@gmail.com>
2023-11-12 14:45:45 -0500

    chore(posts feature): delete all files related to posts feature
    The posts feature was removed from the project as it was no longer needed. This includes all related
    components, API calls, and utilities. This change simplifies the codebase and reduces the overall
    size of the project.

--- src/features/posts/api/get-posts.jsx
+++
@@ -1,6 +0,0 @@
-import { getPostById } from "@/api/posts";
-import { useQuery } from "@tanstack/react-query";
-
-export function GetPostById(id) {
-  return useQuery(["posts", id], () => getPostById(id));
-}
