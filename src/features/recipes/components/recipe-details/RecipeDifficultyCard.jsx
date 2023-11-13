ed6c084c1644eece8da2559833696c9fdc83d480
joekotvas <joseph.kotvas@gmail.com>
2023-11-13 17:21:44 -0500

    fix(recipe difficulty card): hide card when no data available

--- src/features/recipes/components/recipe-details/RecipeDifficultyCard.jsx
+++ src/features/recipes/components/recipe-details/RecipeDifficultyCard.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import ChefCarrots from "@/assets/brand/chef-carrots/ChefCarrots";
 import RecipeDifficultyDetail from "./RecipeDifficultyDetail";
+import { Card } from "./Card";
 
 export const RecipeDifficultyCard = ({ tags }) => {
   // Available cards:
@@ -28,31 +29,39 @@ export const RecipeDifficultyCard = ({ tags }) => {
     (tag) => tag.type === "difficulty" && tag.name === "5_ingredients_or_less",
   );
   return (
-    <div className="relative flex min-h-[6rem] sm:justify-center pl-28 md:pl-[5rem] align-middle">
-      <ChefCarrots
-        resolution="180"
-        className="pointer-events-none select-none w-36 h-56 md:h-auto max-w-[55%] md:bottom-[-3.75rem] md:left-[-4rem] -bottom-6 -left-4 absolute overflow-hidden"
-      />
-      <div className="difficulty-card-wrapper flex flex-col sm:items-center md:items-start lg:items-center sm:flex-row md:flex-col lg:flex-row">
-        {easy && (
-          <RecipeDifficultyDetail
-            title="Easy"
-            icon="easy"
-            text="You've got this!"
+    (easy || quickText || simple) && (
+      <Card className="difficulty-card md:col-start-7 md:col-end-13 md:row-start-2 lg:col-start-5 2xl:col-start-4 flex items-center">
+        <div className="relative flex min-h-[6rem] sm:justify-center pl-28 md:pl-[5rem] align-middle">
+          <ChefCarrots
+            resolution="180"
+            className="pointer-events-none select-none w-36 h-56 md:h-auto max-w-[55%] md:bottom-[-3.75rem] md:left-[-4rem] -bottom-6 -left-4 absolute overflow-hidden"
           />
-        )}
-        {quickText && (
-          <RecipeDifficultyDetail title="Quick" icon="clock" text={quickText} />
-        )}
-        {simple && (
-          <RecipeDifficultyDetail
-            title="Simple"
-            icon="stars"
-            text="5 Ingredients or Less"
-          />
-        )}
-      </div>
-    </div>
+          <div className="difficulty-card-wrapper flex flex-col sm:items-center md:items-start lg:items-center sm:flex-row md:flex-col lg:flex-row">
+            {easy && (
+              <RecipeDifficultyDetail
+                title="Easy"
+                icon="easy"
+                text="You've got this!"
+              />
+            )}
+            {quickText && (
+              <RecipeDifficultyDetail
+                title="Quick"
+                icon="clock"
+                text={quickText}
+              />
+            )}
+            {simple && (
+              <RecipeDifficultyDetail
+                title="Simple"
+                icon="stars"
+                text="5 Ingredients or Less"
+              />
+            )}
+          </div>
+        </div>
+      </Card>
+    )
   );
 };
 
